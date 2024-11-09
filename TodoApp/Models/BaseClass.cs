1cd9b012dbd7384bf253ce6bc77768d39379222f
Eddie <edrismoavenghafouri@gmail.com>
2024-11-10 00:21:08 +0330

    Removing default value

--- TodoApp/Models/BaseClass.cs
+++ TodoApp/Models/BaseClass.cs
@@ -3,10 +3,10 @@ namespace TodoApp.Models;
 public class BaseClass
 {
     public Guid Id { get; set; }
-
-    public DateTime CreatedOn { get; set; } = DateTime.Now;
+    public string Title { get; set; }
+    public DateTime CreatedOn { get; set; }
     public DateTime CreatedBy { get; set; }
 
-    public DateTime UpdatedOn { get; set; } = DateTime.Now;
+    public DateTime UpdatedOn { get; set; }
     public DateTime UpdatedBy { get; set; }
 }
