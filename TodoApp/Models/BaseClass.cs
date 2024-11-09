818d66840cafd66b0e121979d3c7a10b12486992
Eddie <edrismoavenghafouri@gmail.com>
2024-11-09 23:56:37 +0330

    Adding default time to base class

--- TodoApp/Models/BaseClass.cs
+++ TodoApp/Models/BaseClass.cs
@@ -4,9 +4,9 @@ public class BaseClass
 {
     public Guid Id { get; set; }
 
-    public DateTime CreatedOn { get; set; }
+    public DateTime CreatedOn { get; set; } = DateTime.Now;
     public DateTime CreatedBy { get; set; }
 
-    public DateTime UpdatedOn { get; set; }
+    public DateTime UpdatedOn { get; set; } = DateTime.Now;
     public DateTime UpdatedBy { get; set; }
 }
