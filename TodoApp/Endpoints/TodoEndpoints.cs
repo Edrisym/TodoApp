362108e9ea452dcbc12e0e15e939e0889ff50548
Eddie <edrismoavenghafouri@gmail.com>
2024-11-10 23:02:47 +0330

    Creating RESTful endpoints

--- TodoApp/Endpoints/TodoEndpoints.cs
+++ TodoApp/Endpoints/TodoEndpoints.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using TodoApp.Models;
 
 namespace TodoApp.Endpoints;
 
@@ -7,12 +6,66 @@ public static class TodoEndpoints
 {
     public static void MapEndpoints(this WebApplication app)
     {
-        var group = app.MapGroup("/api/v1/todo");
+        var todoGroup = app.MapGroup("/api/v1/todo");
 
 
-        group.MapPost("/", async Task<string> ([FromBody] TodoList todoList) =>
-        {
-            return "";
-        });
+        todoGroup.MapPost("/",
+            async Task<IResult> ([FromBody] TodoList todoList, TodoListDbContext context) =>
+            {
+                try
+                {
+                    await context.TodoLists.AddAsync(todoList);
+                    await context.SaveChangesAsync();
+
+                    return Results.Ok(todoList);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return Results.BadRequest(StatusCodes.Status500InternalServerError);
+                }
+            });
+
+
+        todoGroup.MapGet("/",
+            async Task<IResult> (TodoListDbContext context) =>
+            {
+                try
+                {
+                    var todos = await context.TodoLists.ToListAsync();
+                    return Results.Ok(todos);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return Results.BadRequest(StatusCodes.Status500InternalServerError);
+                }
+            });
+
+        todoGroup.MapPost("/{todoListId:guid}/tasks",
+            async Task<IResult> (Guid todoListId, [FromBody] TaskItem taskItem, TodoListDbContext context) =>
+            {
+                try
+                {
+                    var todoList = await context.TodoLists
+                        .Include(t => t.TaskItems)
+                        .FirstOrDefaultAsync(t => t.Id == todoListId);
+
+                    if (todoList == null)
+                    {
+                        return Results.NotFound(new { message = "TodoList not found" });
+                    }
+
+                    todoList.AddTaskItem(taskItem);
+                    await context.SaveChangesAsync();
+
+                    return Results.Ok(todoList);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return Results.BadRequest(new { error = e.Message });
+                }
+            });
     }
 }
